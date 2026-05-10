1e66f352ff17b1d533b81cf51874cb9b4175e954
s1131439 <s1131439@mail.yzu.edu.tw>
2026-05-10 22:18:20 +0800

    加入專案檔案。

---
+++ WAVPlayer/frmWAVPlayer.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WAVPlayer
+{
+    partial class frmWAVPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpPath = new System.Windows.Forms.GroupBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.grpButton = new System.Windows.Forms.GroupBox();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.ofdWAVFile = new System.Windows.Forms.OpenFileDialog();
+            this.lblFileSize = new System.Windows.Forms.Label();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.grpPath.SuspendLayout();
+            this.grpButton.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpPath
+            // 
+            this.grpPath.Controls.Add(this.btnBrowse);
+            this.grpPath.Controls.Add(this.txtPath);
+            this.grpPath.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpPath.Location = new System.Drawing.Point(12, 12);
+            this.grpPath.Name = "grpPath";
+            this.grpPath.Size = new System.Drawing.Size(528, 97);
+            this.grpPath.TabIndex = 15;
+            this.grpPath.TabStop = false;
+            this.grpPath.Text = "音效位置";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowse.Location = new System.Drawing.Point(421, 37);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(101, 47);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "瀏覽";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtPath.Location = new System.Drawing.Point(6, 43);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(402, 34);
+            this.txtPath.TabIndex = 0;
+            // 
+            // grpButton
+            // 
+            this.grpButton.Controls.Add(this.btnEnd);
+            this.grpButton.Controls.Add(this.btnStop);
+            this.grpButton.Controls.Add(this.btnPlay);
+            this.grpButton.Controls.Add(this.btnLoop);
+            this.grpButton.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpButton.Location = new System.Drawing.Point(12, 125);
+            this.grpButton.Name = "grpButton";
+            this.grpButton.Size = new System.Drawing.Size(528, 97);
+            this.grpButton.TabIndex = 16;
+            this.grpButton.TabStop = false;
+            this.grpButton.Text = "播放按鈕";
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnEnd.Location = new System.Drawing.Point(405, 44);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(115, 47);
+            this.btnEnd.TabIndex = 7;
+            this.btnEnd.Text = "結束程式";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.Location = new System.Drawing.Point(272, 44);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(115, 47);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "停止播放";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.Location = new System.Drawing.Point(6, 44);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(115, 47);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "撥放一次";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnLoop.Location = new System.Drawing.Point(139, 44);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(115, 47);
+            this.btnLoop.TabIndex = 5;
+            this.btnLoop.Text = "重複播放";
+            this.btnLoop.UseVisualStyleBackColor = true;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // ofdWAVFile
+            // 
+            this.ofdWAVFile.DefaultExt = "wav";
+            this.ofdWAVFile.FileName = "openFileDialog1";
+            // 
+            // lblFileSize
+            // 
+            this.lblFileSize.AutoSize = true;
+            this.lblFileSize.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblFileSize.Location = new System.Drawing.Point(18, 330);
+            this.lblFileSize.Name = "lblFileSize";
+            this.lblFileSize.Size = new System.Drawing.Size(72, 25);
+            this.lblFileSize.TabIndex = 19;
+            this.lblFileSize.Text = "大小：";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblFileName.Location = new System.Drawing.Point(18, 285);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(72, 25);
+            this.lblFileName.TabIndex = 18;
+            this.lblFileName.Text = "檔名：";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblStatus.Location = new System.Drawing.Point(18, 240);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(132, 25);
+            this.lblStatus.TabIndex = 17;
+            this.lblStatus.Text = "狀態：未播放";
+            // 
+            // frmWAVPlayer
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 393);
+            this.Controls.Add(this.grpPath);
+            this.Controls.Add(this.grpButton);
+            this.Controls.Add(this.lblFileSize);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "frmWAVPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WAV 音效檔播放器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmWAVPlayer_FormClosing);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmWAVPlayer_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmWAVPlayer_DragEnter);
+            this.grpPath.ResumeLayout(false);
+            this.grpPath.PerformLayout();
+            this.grpButton.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.GroupBox grpButton;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.OpenFileDialog ofdWAVFile;
+        private System.Windows.Forms.Label lblFileSize;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ WAVPlayer/frmWAVPlayer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WAVPlayer
+{
+    public partial class frmWAVPlayer : Form
+    {
+        SoundPlayer player;
+        public frmWAVPlayer()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 當使用者按下「瀏覽」按鈕時，開啟檔案對話框讓使用者選擇 WAV 檔案，並將選擇的檔案路徑顯示在 txtFilePath 文字框中。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            this.ofdWAVFile.Filter = "WAV Files(*.wav)|*.wav";
+            if (this.ofdWAVFile.ShowDialog() == DialogResult.OK)
+            {
+                //this.txtPath.Text = this.ofdWAVFile.FileName;
+                txtPath.Text = ofdWAVFile.FileName;
+
+                FileInfo info = new FileInfo(txtPath.Text);
+
+                lblFileName.Text = "檔名：" + info.Name;
+
+                lblFileSize.Text = "大小：" + info.Length / 1024 + " KB";
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            /*try
+            {
+                player = new SoundPlayer();  //建立撥放器物件
+                player.SoundLocation = txtPath.Text;         //指定音效所在路徑檔名
+                player.Load();                                               //載入音效檔資料
+                player.Play();                                                //撥放音效
+                //player1.PlaySync();                                    //同步撥放音效，直到撥放完成才繼續執行後續程式碼
+                //MessageBox.Show("音效撥放完成！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }*/
+            //沒有選檔案
+            if (string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                MessageBox.Show("請先選擇 WAV 檔案！");
+                return;
+            }
+
+            //檔案不存在
+            if (!File.Exists(txtPath.Text))
+            {
+                MessageBox.Show("檔案不存在！");
+                return;
+            }
+            try
+            {
+                player = new SoundPlayer(txtPath.Text);
+
+                player.Load();
+
+                player.Play();
+
+                lblStatus.Text = "狀態：播放中";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("無法撥放音效檔，請確認檔案路徑是否正確！\n" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLoop_Click(object sender, EventArgs e)
+        {
+            //沒有選檔案
+            if (string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                MessageBox.Show("請先選擇 WAV 檔案！");
+                return;
+            }
+
+            //檔案不存在
+            if (!File.Exists(txtPath.Text))
+            {
+                MessageBox.Show("檔案不存在！");
+                return;
+            }
+            //使用完整檔名建立物件
+            player = new SoundPlayer(txtPath.Text);
+            player.PlayLooping();                                 //重複播放
+            lblStatus.Text = "狀態：循環播放";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //player.Stop();                                        //停止撥放
+            //fsWAV.Close();
+            if (player != null)
+            {
+                player.Stop();
+
+                lblStatus.Text = "狀態：已停止";
+            }
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            //this.Close();
+        }
+
+        private void frmWAVPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("確定要關閉應用程式嗎？", "關閉確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;  //取消關閉
+            }
+        }
+
+        private void frmWAVPlayer_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void frmWAVPlayer_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            txtPath.Text = files[0];
+
+            FileInfo info = new FileInfo(txtPath.Text);
+
+            lblFileName.Text = "檔名：" + info.Name;
+
+            lblFileSize.Text = "大小：" + info.Length / 1024 + " KB";
+        }
+    }
+}
